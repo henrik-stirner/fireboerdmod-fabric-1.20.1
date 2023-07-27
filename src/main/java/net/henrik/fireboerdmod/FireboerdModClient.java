1676fb6b963f183776ee3bb6fef832451bf22e65
Henrik Stirner <henrikstir@gmail.com>
2023-07-27 18:09:51 +0200

    Initial commit

---
+++ src/main/java/net/henrik/fireboerdmod/FireboerdModClient.java
@@ -0,0 +1,10 @@
+package net.henrik.fireboerdmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FireboerdModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
